1e20bd595bf64941204ea6f4e2590ae6337dea52
Frodo B <frodo@mordor.com>
2024-09-03 12:41:51 +0200

    initial commit

---
+++ src/main/java/com/frodo/OllamaAiService.java
@@ -0,0 +1,13 @@
+package com.frodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OllamaAiService {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OllamaAiService.class, args);
+	}
+
+}
